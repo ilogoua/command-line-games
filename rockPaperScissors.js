665b4818b300e91a6a7bd6a898312579ce20dd6a
Anton Bondar <ilogoua@gmail.com>
2026-07-15 16:56:45 +0200

    Complete rock paper scissors game

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,8 +1,8 @@
 const playerMove = process.argv[2];
 const moves = ["rock", "paper", "scissors"];
 if (!moves.includes(playerMove)) {
-  console.log("Please choose rock, paper, or scissors.");
-  process.exit();
+    console.log("Please choose rock, paper, or scissors.");
+    process.exit();
 }
 const randomIndex = Math.floor(Math.random() * moves.length);
 const computerMove = moves[randomIndex];
@@ -17,4 +17,6 @@ if (playerMove === computerMove) {
     (playerMove === 'scissors' && computerMove === 'paper')
 ) {
     console.log('You win!');
+} else {
+    console.log('You lose! :(');
 }
