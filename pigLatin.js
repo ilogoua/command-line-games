1dc08a30533f3ac970a530280279c81f2eebc632
Anton Bondar <ilogoua@gmail.com>
2026-07-17 20:08:39 +0200

    Start Pig Latin translator

---
+++ pigLatin.js
@@ -0,0 +1,9 @@
+const input = process.argv.slice(2).join(" ");
+const words = input.split(" ");
+const firstWord = words[0];
+const firstLetter = firstWord[0];
+
+
+// console.log(words);
+// console.log(firstWord);
+console.log(firstLetter);
